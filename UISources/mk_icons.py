83e48a991d19734e2e4be1687aef56de4830bef5
Eric Schrijver <eric@ericschrijver.nl>
2014-02-06 16:42:56 +0100

    Added the Pink Pony’s Raduga icon for iOS + a script to automatically resize it.

---
+++ UISources/mk_icons.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+import os
+
+"""
+Resize a PDF icon to all icon sizes possibly required for iOS.
+
+Uses OS X’s scriptable image processing system command line utility.
+http://straylink.wordpress.com/2009/01/24/os-x-command-line-image-manipulation/
+
+On Linux, Ghostscript is your best bet!
+"""
+
+icons = [
+    (60, "appicon-60@2x.png"),   # not sure this one is actually used   
+    (120, "appicon-60@2x.png"),
+    (76,  "appicon-76.png"),
+    (152, "appicon-76@2x.png"),
+    (57,  "appicon.png"),
+    (114, "appicon@2x.png"),
+    (72,  "appicon-72.png"),
+    (144,  "appicon-72@2x.png"),
+
+]
+
+for size, filename in icons:
+    outputfile = os.path.join('..', 'Resources', 'iphone', filename)
+    pipe = subprocess.Popen(['sips', '-s',
+                             'format', 'png',
+                             '--resampleWidth', str(size),
+                             'icon.pdf',
+                             '--out', outputfile])
+    pipe.wait()
