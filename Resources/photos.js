dad325ca819b7d665712393787c2e13e2a33d2fc
Eric Schrijver <eric@ericschrijver.nl>
2014-03-17 16:00:46 +0100

    Make the photos look more like the mockup!

--- Resources/photos.js
+++ Resources/photos.js
@@ -7,83 +7,83 @@ Cloud.Photos.query({
     if (e.success) {
         Ti.API.info('found on the internet ' + e.photos.length + ' photos');
         Raduga.photos = e.photos;
-        listView.setSections([createPhotoData()]);
+        tableView.setData(createTableData());
     } else {
         alert('Error:\n' +
             ((e.error && e.message) || JSON.stringify(e)));
     }
 });
 
+var createTableData = function() {
+    var tableData = [];
 
-var plainTemplate = {
-    childTemplates: [
-        {
-            type: 'Ti.UI.Label', // Use a label
-            bindId: 'date',  // Bind ID for this label
-            properties: {        // Sets the Label.left property
-                left: '0dp'
-            }
-        },
-        {
-            type: 'Ti.UI.ImageView',  // Use an image view
-            bindId: 'pic',            // Bind ID for this image view
-            properties: {
-                width: '100%',
-            }
-        },
-        {
-            type: 'Ti.UI.Button',   // Use a button
-            bindId: 'button',       // Bind ID for this button
-            properties: {           // Sets several button properties
-                width: '80dp',
-                height: '30dp',
-                right: '10dp',
-                title: 'press me'
-            },
-            events: { click : function() {
-                // Binds a callback to the button's click event
-            } }
-        }
-    ]
-};
-var listView = Ti.UI.createListView({
-    // Maps the plainTemplate object to the 'plain' style name
-    templates: { 'plain': plainTemplate },
-    // Use the plain template, that is, the plainTemplate object defined earlier
-    // for all data list items in this list view
-    defaultItemTemplate: 'plain'
-});
-
-var createPhotoData = function() {
-    Ti.API.info('creating photo data for ' + Raduga.photos.length + ' photos');
-    var data = [];
     for (var i = 0; i < Raduga.photos.length; i++) {
-        photo = Raduga.photos[i];
+        var photo = Raduga.photos[i];
 
         // Titanium’s cloud service uses the "2014-02-13T14:27:39+0000" format
         // which is not recognised by the Date constructor in iOS
         // The Z is another way of saying GMT.
         photo.created_at = photo.created_at.replace('+0000','Z');
 
-        data.push({
-            // Maps to the rowtitle component in the template
-            // Sets the text property of the Label component
-            date: { text: Date2PonyDate(new Date(photo.created_at)) },
-            pic:  { image: photo.urls.thumb_100 },
-            hour: { text: Date2PonyHour(new Date(photo.created_at)) },
-            // Sets the regular list data properties
-            properties : {
-                itemId: 'row' + (i + 1),
-                accessoryType: Ti.UI.LIST_ACCESSORY_TYPE_NONE
-            }
+        var row = Ti.UI.createTableViewRow({
+            className: 'rainbowPhoto', // used to improve table performance
+            backgroundColor: 'black',
+            // selectedBackgroundColor: 'white',
+            rowIndex: i, // custom property, useful for determining the row during events
         });
+
+        var imageAvatar = Ti.UI.createImageView({
+            image: photo.urls.medium_640,
+            left: 0,
+            top: 0,
+            width: '320dp'
+        });
+        row.add(imageAvatar);
+
+        tableData.push(row);
+
+        var row = Ti.UI.createTableViewRow({
+            className: 'rainbowMeta', // used to improve table performance
+            backgroundColor: 'black',
+            // selectedBackgroundColor: 'black',
+            rowIndex: i, // custom property, useful for determining the row during events
+            height: '20dp',
+            layout:'horizontal'
+        });
+
+        var labelHour = Ti.UI.createLabel({
+            color: 'white',
+            text: Date2PonyHour(new Date(photo.created_at)),
+            width: Titanium.UI.SIZE,
+            left: '10dp'
+        });
+        row.add(labelHour);
+
+        var labelDate = Ti.UI.createLabel({
+            color: 'white',
+            text: Date2PonyDate(new Date(photo.created_at)),
+            width: Titanium.UI.SIZE,
+            left: '10dp'
+        });
+        row.add(labelDate);
+
+        var labelUserName = Ti.UI.createLabel({
+            color: 'white',
+            text: photo.user.username,
+            width: Titanium.UI.SIZE,
+            left: '10dp'
+
+        });
+        row.add(labelUserName);
+
+        tableData.push(row);
     }
-    return Ti.UI.createListSection({items: data});
+    return tableData;
 };
 
-listView.sections = [createPhotoData()];
-listView.addEventListener('photosFetched', function(e){
-    listView.setSections([createPhotoData()]); //doesnt work yet
+var tableView = Ti.UI.createTableView({
+    backgroundColor: 'white',
+    data: createTableData()
 });
 
-photosWindow.add(listView);
+photosWindow.add(tableView);
