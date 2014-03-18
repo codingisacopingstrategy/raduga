ef926bd2b35349b10da3b960ba748b5216c73452
Eric Schrijver <eric@ericschrijver.nl>
2014-03-18 16:02:59 +0100

    Only call the camera once; afterwards put produced image in a view
    TODO: make the upload form
    
    The camera was getting called each time the tab had focus, and had gotten into a loop

--- Resources/camera.js
+++ Resources/camera.js
@@ -1,7 +1,29 @@
+var usernameLabel = Titanium.UI.createLabel({
+    color: '#821785',
+    text: 'User Name',
+    top: 10, left: 10,
+    width: 250
+});
+
+// Set up screen
+
+var cameraLabel = Titanium.UI.createLabel({
+    color: '#821785',
+    text: 'Take picture',
+    top: '10dp', left: '10dp', right: '10dp'
+});
+cameraWindow.add(cameraLabel);
+Raduga.callingCamera = false; // this might not be save across threads
 // Camera Behaviour
 
 // from the example http://docs.appcelerator.com/titanium/3.0/#!/guide/Camera_and_Photo_Gallery_APIs :
 var showCam = function() {
+    if (Raduga.callingCamera) {
+        Ti.API.info("called showCam, but not listening");
+        return null;
+    }
+    Raduga.callingCamera = true;
+    Ti.API.info("called showCam");
     Titanium.Media.showCamera({
         success:function(event) {
             // called when media returned from the camera
@@ -30,8 +52,9 @@ var showCam = function() {
             }
             a.show();
         },
-        saveToPhotoGallery:true,
-        allowEditing:true,
+        saveToPhotoGallery: true,
+        allowEditing: true,
+        autoHide: true,
         mediaTypes:[Ti.Media.MEDIA_TYPE_VIDEO,Ti.Media.MEDIA_TYPE_PHOTO]
     });
 };
