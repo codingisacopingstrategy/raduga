7bd75f6800ca4b3b72d679b4681c1a0af60414d9
Eric Schrijver <eric@ericschrijver.nl>
2014-03-23 18:51:09 +0100

    Create a web view that shows the globe.
    The globe is an image from the Elektro L satellite.
    We will be able to get new ones in near real-time.
    
    The globe is then overlaid with a graticule, country borders, and soon:
    information about rainbows.

--- Resources/globe.js
+++ Resources/globe.js
@@ -1,4 +1,5 @@
 // Behaviour of globeWindow, The globe with the rainbows
+var u = Ti.Android != undefined ? 'dp' : 0;
 
 var image = Ti.UI.createImageView({
     image:'/ui/mock_03.png',
@@ -10,5 +11,14 @@ var container =  Titanium.UI.createView({
     height: '100%',
     borderRadius: 0 // https://developer.appcelerator.com/question/23931/imageview-scaling-mode
 });
+
+var globe = Titanium.UI.createWebView({
+    top: '20dp',
+    backgroundImage: 'html/elektro_l_20140311_0530_rgb.jpg',
+    url: 'html/globe.html',
+    width: Ti.Platform.displayCaps.platformWidth + u,
+    height: Ti.Platform.displayCaps.platformWidth + u
+});
+
 container.add(image);
-globeWindow.add(container);
+globeWindow.add(globe);
