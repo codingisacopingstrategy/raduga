a976b9dbbf11518ff17d719dad4620b3b08a28a0
Eric Schrijver <eric@ericschrijver.nl>
2014-05-15 21:07:41 +0200

    When loading the map, center on users home town.

--- Resources/map.js
+++ Resources/map.js
@@ -1,3 +1,10 @@
 var mapWebView = Ti.UI.createWebView({url:'html/index.html'});
 
+mapWebView.addEventListener('load', function() {
+    // If someon clicks on the map before they set up their location; center on Moscow
+    var city = Ti.App.Properties.getString('city_name_en') ? Ti.App.Properties.getString('city_name_en') : 'Moscow';
+    Ti.API.info("Centering map on " + city);
+    mapWebView.evalJS('initMap("' + city +'");');
+});
+
 mapWindow.add(mapWebView);
