1edf8228729ce81658249a84fc37da9d063b321f
Eric Schrijver <eric@ericschrijver.nl>
2014-06-02 13:08:26 +0200

    Don’t make map bounce around when navigating
    still need to fix the width on iPad

--- Resources/map.js
+++ Resources/map.js
@@ -1,7 +1,15 @@
-var mapWebView = Ti.UI.createWebView({url:'html/index.html'});
+var u = Ti.Android != undefined ? 'dp' : 0;
+
+var mapWebView = Ti.UI.createWebView({
+    url : 'html/index.html',
+    width: '100%',
+    width: Ti.Platform.displayCaps.platformWidth + u,
+    height: Ti.Platform.displayCaps.platformHeight + u,
+    disableBounce: true
+});
 
 mapWebView.addEventListener('load', function() {
-    // If someon clicks on the map before they set up their location; center on Moscow
+    // If someone clicks on the map before they set up their location; center on Moscow
     var city = Ti.App.Properties.getString('city_name_en') ? Ti.App.Properties.getString('city_name_en') : 'Moscow';
     Ti.API.info("Centering map on " + city);
     mapWebView.evalJS('initMap("' + city +'");');
