cac28c101c2221d92d9a87172f1de3aedce94b22
Eric Schrijver <eric@ericschrijver.nl>
2014-06-05 12:46:11 +0200

    Re-order the settings and user files so we can get the button underneath—
    and make the button nicely visible

--- Resources/settings.js
+++ Resources/settings.js
@@ -37,13 +37,79 @@ var topPadding = Ti.UI.createView({
     height: '20dp'
 });
 
-var userDialog = Ti.UI.createView({
-   top:0,
-   height: Ti.UI.SIZE,
-   layout: 'vertical',
+/* View code */
+var activityIndicator = Ti.UI.createActivityIndicator({
+    top: 0,
+    left: 0,
+    height: '10dp',
+    width: '10dp'
 });
 
-updateUserDialog(userDialog);
+var usernameLabel = Ti.UI.createLabel({
+    color: 'black',
+    textid: 'username',
+    top: '10dp', left: '10dp',
+    width: '250dp'
+});
+
+var usernameTextField = Ti.UI.createTextField({
+    borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+    top: '10dp', left: '10dp',
+    width: '250dp'
+});
+
+var passwordLabel = Ti.UI.createLabel({
+    color: 'black',
+    textid: 'password',
+    top: '10dp', left: '10dp',
+    width: '250dp'
+});
+
+var passwordTextField = Ti.UI.createTextField({
+    borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+    top: '10dp', left: '10dp',
+    width: '250dp',
+    passwordMask: true
+});
+
+var passwordCheckLabel = Ti.UI.createLabel({
+    color: 'black',
+    textid: 'password_again',
+    top: '10dp', left: '10dp',
+    width: '250dp'
+});
+
+var passwordCheckTextField = Ti.UI.createTextField({
+    borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+    top: '10dp', left: '10dp',
+    width: '250dp',
+    passwordMask: true
+});
+
+var loginButton = Ti.UI.createButton({
+   titleid: 'login',
+   top: 10,
+   width: 100
+});
+
+var logoutButton = Ti.UI.createButton({
+   titleid: 'logout',
+   top: 10,
+   width: 100
+});
+
+
+var signupButton = Ti.UI.createButton({
+   titleid: 'signup',
+   top: 10,
+   width: 100
+});
+
+if (Raduga.Platform.ios) {
+    loginButton.setStyle(Ti.UI.iPhone.SystemButtonStyle.BORDERED);
+    logoutButton.setStyle(Ti.UI.iPhone.SystemButtonStyle.BORDERED);
+    signupButton.setStyle(Ti.UI.iPhone.SystemButtonStyle.BORDERED);
+}
 
 var notificationsLabel = Ti.UI.createLabel({
     color: 'black',
@@ -81,14 +147,7 @@ var settingsScrollView = Ti.UI.createScrollView({
    showHorizontalScrollIndicator:true
 });
 
-settingsScrollView.add(activityIndicator);
 settingsWindow.add(settingsScrollView);
-
-settingsScrollView.add(userDialog);
-settingsScrollView.add(cityLabel);
-settingsScrollView.add(cityTextField);
-settingsScrollView.add(notificationsLabel);
-settingsScrollView.add(notificationsSwitch);
 
 //
 // Saving settings
@@ -161,7 +220,70 @@ cityTextField.addEventListener('focus', function(e) {
     citiesSearch.focus();
 });
 
+// setup user actions
+logoutButton.addEventListener('click', logoutUser);
+
+var handleSignup = function() {
+    createUser(usernameTextField.value, passwordTextField.value, passwordCheckTextField.value);
+};
+signupButton.addEventListener('click', handleSignup);
+
+var handleLogin = function() {
+    loginUser(usernameTextField.value, passwordTextField.value);
+};
+loginButton.addEventListener('click', handleLogin);
+
+
 // handle settings refresh
 settingsWindow.addEventListener('user_status_change', function() {
-    updateUserDialog(userDialog);
+    updateUserDialog(settingsScrollView);
 });
+
+var updateUserDialog = function(view) {
+    view.removeAllChildren(); //TODO: also remove event listeners
+
+    if (signedUp()) {
+        usernameTextField.value = Ti.App.Properties.getString('username');
+        if (loggedIn()) {
+            usernameTextField.setEnabled(false); // Android only
+            usernameTextField.setEditable(false);
+        }
+    } else {
+        usernameTextField.value = '';
+        usernameTextField.setEnabled(true); // Android only
+        usernameTextField.setEditable(true);
+    }
+
+    view.add(activityIndicator);
+    view.add(usernameLabel);
+    view.add(usernameTextField);
+
+    if (!loggedIn()) {
+        // We are not logged in. Add the pass
+        view.add(passwordLabel);
+        view.add(passwordTextField);
+        if (!signedUp()) {
+            view.add(passwordCheckLabel);
+            view.add(passwordCheckTextField);
+        }
+    }
+
+    view.add(cityLabel);
+    view.add(cityTextField);
+    view.add(notificationsLabel);
+    view.add(notificationsSwitch);
+
+
+    if (loggedIn()) {
+        view.add(logoutButton);
+    } else {
+        if (!signedUp()) {
+            view.add(signupButton);
+        } else {
+            view.add(loginButton);
+        }
+    }
+
+};
+
+updateUserDialog(settingsScrollView);
