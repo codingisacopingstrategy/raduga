36936c0244f200def4630a7b1ca1c0cdcff0d52b
Eric Schrijver <eric@ericschrijver.nl>
2014-06-11 07:49:28 +0200

    Internationalise push messages—should test this, but the certificate got revoked?

--- Resources/push_ios.js
+++ Resources/push_ios.js
@@ -16,6 +16,20 @@ var setupPush = function (success) {
             alertError('Failed to register for iOS push notifications! ' + e.error);
         },
         callback: function(e) {
+            var message;
+            if (e.data.type === "rainbow_prediction") {
+                if (new Date().getHours() < 12) {
+                    message = L('rainbow_predicted_morning');
+                } else {
+                    message = L('rainbow_predicted_afternoon');
+                }
+            } else if (e.data.type === "rainbow_spotted") {
+                message = String.format(L('photo_caption'),
+                e.data[Raduga.Platform.currentLanguage === 'ru' ? 'name_ru' : 'name_en'],
+                e.data.username);
+            } else {
+                return false;
+            }
             var dialog = Ti.UI.createAlertDialog({
                 message: e.data.alert + JSON.stringify(e),
                 ok: 'OK',
