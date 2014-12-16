cfbeefdc4aa07ce1dad1eed288862cb75295c42f
Eric Schrijver <eric@ericschrijver.nl>
2014-12-16 17:04:05 +0100

    Commenting run on app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,10 +1,28 @@
-var Raduga = {};
+/*
+ * The main app file
+ *
+ * The site is implemented as a tabbed application.
+ * Each tab is connected to a Window.
+ *
+ * The achieve modularity, the app uses the CommonJS module system.
+ * Each JavaScript file exposes certain functions.
+ * A file can be loaded in with a call to require();
+ *
+ */
+
+//
+// Tell us where we are
+//
 
 var Platform = require('platform');
 Ti.API.info(["OS:       " + Platform.osname,
              "Language: " + Platform.currentLanguage,
              "Screen:   " + Platform.width + "*" + Platform.height].join("\n"));
-var UI = require('ui');
+
+
+//
+// Set up Push notifications
+//
 
 var Cloud = require('ti.cloud');
 // Persist the session from last time so we don’t have to login again,
@@ -33,36 +51,42 @@ var push = new pushlib.Push(function(deviceToken) {
     });
 });
 
-var cities = require('cities').cities;   // the coordinates of 1100 Russian towns and cities
-var gradients = require('gradients');
-var utils = require('utils');
-
-var users = require('users');
-
-/* Set up UI */
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Ti.UI.setBackgroundColor('#ffffff');
-// launch in portrait mode
-Ti.UI.orientation = Ti.UI.PORTRAIT;
 //
-// Create the Windows
+// Require utility functions
+//
+
+var UI = require('ui');                  // functions to make UI elements more easily
+var cities = require('cities').cities;   // the coordinates of 1100 Russian towns and cities
+var gradients = require('gradients');    // the app uses background gradients that change with time
+var utils = require('utils');            // various utility functions
+var users = require('users');            // allows, among other things, to see if the user is logged in
+
+//
+// Require the modules for the five main windows
 //
 
 var globelib = require('globe');
 var globe = new globelib.Globe();
-
 var photoslib = require('photos');
 var photos = new photoslib.Photos();
-
 var maplib = require('map');
 var map = new maplib.Map();
-
 var cameralib = require('camera');
 var camera = new cameralib.Camera();
-
 var settingslib = require('settings');
 var settings = new settingslib.Settings();
 
+//
+// Set UI defaults
+//
+
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Ti.UI.setBackgroundColor('#000000');
+// launch in portrait mode
+Ti.UI.orientation = Ti.UI.PORTRAIT;
+
+
+// Set appropriate status bar style (light or dark text) for each tab
 if (Platform.ios) {
     photos.window.setStatusBarStyle(Ti.UI.iPhone.StatusBar.DARK_CONTENT);
     photos.window.setExtendEdges([Ti.UI.EXTEND_EDGE_TOP, Ti.UI.EXTEND_EDGE_BOTTOM]);
@@ -80,15 +104,12 @@ if (Platform.ios) {
 // Create tabs that will house the windows, and associated events
 //
 
+// To make the tabs semi-transparent I had to use this one-pixel background image on iOS
 var tabGroup = Ti.UI.createTabGroup({
     tabsBackgroundImage: 'ui/backgrounds/semi-transparant-pixel-60.png',
-    // I want to not have the default tab icons be coloured grey. Both of these have no effect:
-    // cf http://developer.appcelerator.com/question/163061/tab-group-grey-icons-in-io7
     tabsBackgroundColor: 'transparent',
-    // tabsBackgroundDisabledColor: 'white',
     tintColor: '#929292',
     navBarHidden: true,
-
 });
 
 var photosTab = Ti.UI.createTab({
@@ -98,16 +119,6 @@ var photosTab = Ti.UI.createTab({
     window: photos.window,
     width: '20%',
     height: '50dp'
-});
-
-photosTab.addEventListener("focus", function(e) {
-    Ti.API.info("focus on the photosTab");
-    if (e.source.id === 'photosTab') {
-        Ti.API.info("focus on the photosTab tab, updating photos");
-        photos.update();
-    } else {
-        Ti.API.info("Not actually update phototab");
-    }
 });
 
 var globeTab = Ti.UI.createTab({
@@ -126,7 +137,6 @@ var mapTab = Ti.UI.createTab({
     height: '50dp'
 });
 
-// for now we add it as a tab, but it will probably be a seperate button on the top of the screen
 var cameraTab = Ti.UI.createTab({
     id: 'cameraTab',
     icon: 'ui/icons/camera.png',
@@ -134,15 +144,6 @@ var cameraTab = Ti.UI.createTab({
     window: camera.window,
     width: '20%',
     height: '50dp'
-});
-
-// Get the camera
-cameraTab.addEventListener("focus", function(e){
-    Ti.API.info("focus on the cameraTab");
-    if (e.source.id === 'cameraTab') {
-        Ti.API.info("focus on the cameraTab tab");
-        camera.showCam();
-    }
 });
 
 var settingsTab = Ti.UI.createTab({
@@ -158,6 +159,32 @@ tabGroup.addTab(mapTab);
 tabGroup.addTab(cameraTab);
 tabGroup.addTab(photosTab);
 tabGroup.addTab(settingsTab);
+
+//
+// Setup UI events and other events
+//
+
+// Tab events
+
+photosTab.addEventListener("focus", function(e) {
+    Ti.API.info("focus on the photosTab");
+    if (e.source.id === 'photosTab') {
+        Ti.API.info("focus on the photosTab tab, updating photos");
+        photos.update();
+    } else {
+        Ti.API.info("Not actually update phototab");
+    }
+});
+
+cameraTab.addEventListener("focus", function(e){
+    Ti.API.info("focus on the cameraTab");
+    if (e.source.id === 'cameraTab') {
+        Ti.API.info("focus on the cameraTab tab");
+        camera.showCam();
+    }
+});
+
+// Application wide events. Are triggered by Ti.App.fireEvent('eventName')
 
 Ti.App.addEventListener('switchTab', function(e) {
     if (e.tab === "photos") {
@@ -201,32 +228,29 @@ Ti.App.addEventListener('spottedRainbows', function(e) {
 });
 
 Ti.App.addEventListener('rainbowClicked', function(e) {
-    // The underlying idea is that the index set in map.js > photos2Features
-    // equates to the rowIndex set in photos.js > createTableData
-    // and that that thus the photoscroll can scroll to the position of
-    // the corresponding photo, when a city is clicked on the map
-    // however, FIXME ’cause this doesn’t seem to work
-    // the photoscroll does scroll, simply not to the right place
+    // If a rainbow has been clicked on the map this scrolls to the corresponding photo in the photoscroll
+    // Because the rainbows on the map are created from the same array as the photos on the photoscroll
+    // the nth rainbow should correspond to the nth photo in the photoscroll
+    //
+    // we use scrolltoIndex which goest to the nth element in the photoscroll
+    // however, before each photo there is a separator element
+    // so to get to nth photo we scroll to the n*2 + 1th element
     tabGroup.setActiveTab(photosTab);
     Ti.API.info('click on index ' + e.index );
-    Ti.API.info('scrolling to image, I hope');
+    Ti.API.info('scrolling to image');
     var animationOptions = Ti.Platform.ios ? { animated: true, position: Ti.UI.iPhone.TableViewScrollPosition.TOP} : null;
     photos.tableView.scrollToIndex(parseInt(e.index, 10) * 2 + 1, animationOptions);
 });
 
 
-/*
- * ATTENTION: extremely inelegant way of making sure the mapTab background
- * is more opaque
- *
- */
+// The background on tabbed bar needs to be a bit less transparent on when we are on the the map tab,
+// because otherwise the buttons become hard to read. This is a rather inelegant way of accomplishing this
 var standardOp = function() {
     tabGroup.setTabsBackgroundImage('ui/backgrounds/semi-transparant-pixel-60.png');
 };
 var highOp = function() {
     tabGroup.setTabsBackgroundImage('ui/backgrounds/semi-transparant-pixel-80.png');
 };
-
 cameraTab.addEventListener("focus", standardOp);
 globeTab.addEventListener("focus", standardOp);
 mapTab.addEventListener("focus", highOp);
@@ -234,7 +258,7 @@ cameraTab.addEventListener("focus", standardOp);
 settingsTab.addEventListener("focus", standardOp);
 
 //
-// Initialise app
+// Initialise the app
 //
 
 var initWithNetwork = function() {
@@ -257,7 +281,7 @@ if (Ti.Network.getNetworkTypeName() === "NONE") {
     initWithNetwork();
 }
 
-// If connection drops or becomes available
+// Setup behaviour for if connection drops or becomes available
 Ti.Network.addEventListener('change', function(e) {
     Ti.API.info("detected change in network connectivity");
     if (Ti.Network.getNetworkTypeName() === "NONE") {
@@ -267,13 +291,19 @@ Ti.Network.addEventListener('change', function(e) {
     }
 });
 
+//
+// And finally, keep changing UI colours with day time
+//
+
+var currentGradientslug;
+
 var updateColours = function() {
     if (Ti.Network.getNetworkTypeName() === "NONE") {
         // no internet access, no updates
         return;
     }
     Ti.API.info("Checking if colours need to be updated");
-    if (Raduga.currentGradientslug === gradients.gradientSlug()) {
+    if (currentGradientslug === gradients.gradientSlug()) {
         return null;
     }
     Ti.API.info("colours need to be updated");
@@ -282,7 +312,7 @@ var updateColours = function() {
     globe.updateColours();
     settings.updateColours();
 
-    Raduga.currentGradientslug = gradients.gradientSlug();
+    currentGradientslug = gradients.gradientSlug();
 };
 
 var colourTimer = setInterval(updateColours, 300000);
